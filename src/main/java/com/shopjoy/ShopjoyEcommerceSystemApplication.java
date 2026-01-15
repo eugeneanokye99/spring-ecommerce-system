194f6437806ae4186070b292ba5115bf0b3ae429
EugeneDokyeAnokye <yawanokye99@gmail.com>
2026-01-15 17:47:44 +0000

    initial commit

---
+++ src/main/java/com/shopjoy/ShopjoyEcommerceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shopjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopjoyEcommerceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopjoyEcommerceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopjoy/ShopjoyEcommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopjoyEcommerceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
